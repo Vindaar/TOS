25638cc5f1bd3f4ebf788553a7a1dcbc7b931187
Sebastian Schmidt <basti90@gmail.com>
2016-04-21 15:00:50 +0200

    changed static_cast to reinterpret_cast in networkWrapper (unrelated types char* and unsigned char*)

--- src/networkWrapper.cpp
+++ src/networkWrapper.cpp
@@ -16,18 +16,18 @@ int sendWrapper(int sock, unsigned char* SendBuffer, int OutgoingLength, int fla
     // after calling the function convert the result back to an unsigned char*
     int recvBytes;
 
-#ifdef __WIN32__
+//#ifdef __WIN32__
     // create a char * windows variable
     char *winSendBuffer;
     // perform a static cast from unsigned to signed char*
-    winSendBuffer = static_cast<char *> SendBuffer;
+    winSendBuffer = reinterpret_cast<char *> (SendBuffer);
     // call windows function with char *
     recvBytes = send(sock, winSendBuffer, OutgoingLength, flags);
     // convert char * back to unsigned char *
-    SendBuffer = static_cast<unsigned char *> winSendBuffer;
-#else
+    SendBuffer = reinterpret_cast<unsigned char *> (winSendBuffer);
+//#else
     recvBytes = send(sock, SendBuffer, OutgoingLength, flags);
-#endif
+//#endif
     
     return recvBytes;
 }
@@ -48,15 +48,15 @@ int recvWrapper(int sock, unsigned char* RecvBuffer, int IncomingLength, int fla
     // after calling the function convert the result back to an unsigned char*
     int recvBytes;
 
-#ifdef __WIN32__
+//#ifdef __WIN32__
     // same procedure as in sendWrapper()
     char *winRecvBuffer;
-    winRecvBuffer = static_cast<char *> RecvBuffer;
+    winRecvBuffer = reinterpret_cast<char *> (RecvBuffer);
     recvBytes = recv(sock, winRecvBuffer, IncomingLength, 0);
-    RecvBuffer = static_cast<unsigned char*> winRecvBuffer;
-#else
+    RecvBuffer = reinterpret_cast<unsigned char*> (winRecvBuffer);
+//#else
     recvBytes = recv(sock, RecvBuffer, IncomingLength, 0);
-#endif     
+//#endif     
 
     return recvBytes;
 }
