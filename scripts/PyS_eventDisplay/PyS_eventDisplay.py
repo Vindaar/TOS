bug fix regarding active pixels in event display (factor of 3, because we used chip data array as quick fix, since we didn't want pixels at 11810 counts)

scripts/PyS_eventDisplay/PyS_eventDisplay.py
@@ -11,7 +11,7 @@ import os
 import time
 from septemClasses import chip, septem_row, septem, eventHeader, chipHeaderData, Fadc
 from septemFiles import create_files_from_path_combined, read_zero_suppressed_data_file
-from septemPlot  import plot_file, plot_fadc_file, plot_occupancy
+from septemPlot  import plot_file, plot_fadc_file, plot_occupancy, plot_pixel_histogram
 
 import multiprocessing as mp
 from profilehooks import profile
@@ -222,6 +222,10 @@ class WorkOnFile:
             # if o is typed, we create an occupancy plot of the whole run
             print 'creating occupancy plot...'
             self.create_occupancy_plot()
+        elif c == 'h':
+            # if h is typed, we create the pixel histogram for each chip
+            print 'creating pixel histogram...'
+            self.create_pixel_histogram()
                         
                 
         elif c == 'q':
@@ -319,6 +323,41 @@ class WorkOnFile:
                        self.cb_flag,
                        self.cb_value,
                        self.cb_chip)
+
+
+    def create_pixel_histogram(self):
+        # create the pixel histogram for all chips of the current run
+        
+        # define a list of lists for the number of hits for each chip
+        nHitsList = [ [] for _ in xrange(self.septem.nChips)]
+
+        for el in self.ns.filelistEvents:
+            # we create the event and chip header object
+            evHeader, chpHeaderList = read_zero_suppressed_data_file(self.filepath + el)
+            # now go through each chip header and add data of frame
+            # to chip_arrays
+            for chpHeader in chpHeaderList:
+                # get the data of the frame for this chip
+                chip_data = chpHeader.pixData
+                chip_num  = int(chpHeader.attr["chipNumber"])
+                # and now add chip data to chip_arrays if non empty
+                nHits = np.size(chip_data)
+                if nHits > 0:
+                    # now if we have 1 hits or more, add event to histogram
+                    # chipnum - 1, because we count from 1
+                    nHitsList[chip_num - 1].append(nHits)
+                
+            event_num = int(evHeader.attr["eventNumber"])
+            if event_num % 1000 == 0:
+                print event_num, ' events done.' 
+
+        plot_pixel_histogram(self.filepath,
+                             self.fig,
+                             self.septem,
+                             self.chip_subplots,
+                             self.im_list,
+                             nHitsList,
+                             self.single_chip_flag)
         
 
 

scripts/PyS_eventDisplay/septemPlot.py
@@ -9,8 +9,9 @@ from septemClasses import Fadc
 from profilehooks import profile
 
 
-def make_ticklabels_invisible(fig):
-    for i, ax in enumerate(fig):
+def make_ticklabels_invisible(subplots):
+    
+    for i, ax in enumerate(subplots):
         #ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
         for tl in ax.get_xticklabels() + ax.get_yticklabels():
             tl.set_visible(False)
@@ -67,8 +68,8 @@ def plot_file(filepath, filename, sep, fig, chip_subplots, im_list, cb_flag, cb_
             # counting chips at 1.
             break
 
-        # and get the number of hits
-        numHits = int(np.size(chip_data))#chpHeader.attr["numHits"])
+        # and get the number of hits (we use numHits)
+        numHits = chpHeader.attr["numHits"]) # int(np.size(chip_data))
         # use both to create the hits box
         hits_text += "Chip #%i : %i" % (chipNum, numHits)
         if chipNum != 7:
@@ -119,6 +120,8 @@ def plot_file(filepath, filename, sep, fig, chip_subplots, im_list, cb_flag, cb_
         for i in plots_to_hide:
             im_list[i].set_visible(False)
 
+    print 'number of axes1', fig.get_axes()
+
     try:
         # either set the colorbar to the center chip in case of the septemboard or 
         # to chip 0 in case of a single chip
@@ -133,6 +136,10 @@ def plot_file(filepath, filename, sep, fig, chip_subplots, im_list, cb_flag, cb_
         fig.canvas.draw()
     except UnboundLocalError:
         print filename
+
+    axes = fig.get_axes()
+    for el in axes:
+        print matplotlib.artist.getp(el)
     
     make_ticklabels_invisible(chip_subplots)
     #fig.canvas.draw()
@@ -284,3 +291,93 @@ def plot_occupancy(filepath,
 
     # and now plot everythin
     plt.pause(0.01)
+
+
+def plot_pixel_histogram(filepath,
+                         fig,
+                         sep,
+                         chip_subplots,
+                         im_list,
+                         nHitsList,
+                         single_chip_flag):
+
+    # this function plots the pixel histogram, i.e. a histogram of the number
+    # of pixels active in an event of the whole run
+
+    # define nChips based on the chip_subplots list, to differentiate between
+    # single chip and septemboard
+    nChips = len(chip_subplots)
+    
+    # first remove the texts from before, if any
+    texts = fig.texts
+    for i in range(len(texts)):
+        texts[-1].remove()
+
+    # define the variable, in which we store the number of hits for each chip to
+    # print at the top left
+    # hits_text = "".ljust(10) + "Hits".ljust(10) + "max values\n"
+
+    # now perform plotting
+    plots_to_hide = range(7)
+    print nChips, 'h'
+    for i, hitsList in enumerate(nHitsList):
+        # using the iterator i, we determine if we break or not
+        # (relevant for single chip plotting. Then we don't want to plot more than chip #1)
+
+        if i >= nChips:
+            break
+
+        # now either plot a single plot (if single chip), or one for each
+        # chip
+        #if single_chip_flag is False:
+        print i
+        #print hitsList
+        im_list[i].set_visible(False)
+        
+        #fig2 = plt.figure(2)
+        
+        #plt.hist(hitsList, 100, histtype = 'bar')
+        pos = chip_subplots[i].get_position()
+        print pos
+
+        print pos.x0, pos.y0, pos.x1, pos.y1
+        histo = fig.add_axes([pos.x0, pos.y0, pos.width, pos.height])
+        histo.hist(hitsList, 20, histtype = 'bar')
+
+        #chip_subplots[i].hist(hitsList, 100, histtype = 'bar')
+        #else:
+            
+
+        # hits_text += ("Chip #%i : %i" % (i, numHits)).ljust(20)
+        # hits_text += str(int(maxVals))
+        #if i != 6:
+        #    hits_text += "\n"
+
+        # TODO: still need plots_to_hide for case in which histogram of one chip
+        # might be empty?
+        # plots_to_hide.remove(i)
+
+
+    # now create the hits box
+    # hits_box = fig.text(0.2, 0.9, hits_text,
+    #                     bbox={'facecolor':'blue', 'alpha':0.1, 'pad':15},
+    #                     family = 'monospace',
+    #                     transform = chip_subplots[-1].transAxes,
+    #                     horizontalalignment = 'center',
+    #                     verticalalignment = 'center',
+    #                     multialignment = 'now')
+
+
+
+    fig.canvas.draw()
+
+    # left set all plots invisible, which were not updated this time (only if septemboard)
+    #if nChips > 1:
+    for i in xrange(nChips):
+        im_list[i].set_visible(False)
+    
+    make_ticklabels_invisible(chip_subplots)
+
+    # and now plot everythin
+    plt.pause(0.01)
+    