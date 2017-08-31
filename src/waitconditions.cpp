Vindaar/TOS

commit bed62790d44e51678472e69ffde14bd687c8f18f

refactor FADC code dealing with active FADC channels
Refactors parts of FADC code, which calculated number of active
channels based on the channel mask. New function, which performs the
calculation is added.

src/waitconditions.cpp
@@ -178,19 +178,14 @@ void Producer::run()
 		fadcParams["scint2ClockInt"] = scint_pair.second;
 		
 		//get nb of channels
-		int channels = 4;
+		unsigned short channels = 4;
 
 		if( (fadcParams["NumChannels"] !=1 ) && (fadcParams["NumChannels"] !=2) ){
-		    if( !(fadcParams["ChannelMask"] & 8) ) channels--;
-		    if( !(fadcParams["ChannelMask"] & 4) ) channels--;
-		    if( !(fadcParams["ChannelMask"] & 2) ) channels--;
-		    if( !(fadcParams["ChannelMask"] & 1) ) channels--;
+		    channels = parent->_hvFadcManager->FADC_Functions->getNumberOfActiveChannels(fadcParams["channelMask"]);
 		}
 
 		//get fadc data
-		//TODO/FIXME one wants to use channels instead of 4 as parameter of the next function?
-		// TODO: fix this!!!
-		std::vector<int> fadcData = (parent->_hvFadcManager)->F_GetAllData(4);
+		std::vector<int> fadcData = (parent->_hvFadcManager)->F_GetAllData(channels);
 		std::cout << "saving data to parent pointer. will probably fail " << std::endl;
 		// set the data we read from the FADC as the values for our fadc pointer
 		parent->_fadcData   = fadcData;

include/High-Level-functions_VME.h
@@ -26,104 +26,104 @@
 //class HighLevelFunction_VME : public HighLevelFunction{
 class HighLevelFunction_VME{
 
- public:
+public:
   
-  //TODO: Write a load settings from file function
+    //TODO: Write a load settings from file function
 
-  /// C'tor
-  HighLevelFunction_VME(V1729a* dev);
+    /// C'tor
+    HighLevelFunction_VME(V1729a* dev);
 
-  /// D'tor
-  ~HighLevelFunction_VME();
+    /// D'tor
+    ~HighLevelFunction_VME();
 
 
 
-  /*********** General Functions ***********/
+    /*********** General Functions ***********/
   
 
 
-  /** Prints the actual settings of the FADC to
-   * console or to a file
-   **/
-  void printSettings();
+    /** Prints the actual settings of the FADC to
+     * console or to a file
+     **/
+    void printSettings();
 
 
 
-  /*********** Set Functions ***********/
+    /*********** Set Functions ***********/
    
   
   
-  /** Since the FADC fails from time to time to set some parameters
-   * to theire values, a few set functions were rewritten
-   * to ensure in _nb trys, that the parameters are set in 
-   * the intendet way
-   **/  
+    /** Since the FADC fails from time to time to set some parameters
+     * to theire values, a few set functions were rewritten
+     * to ensure in _nb trys, that the parameters are set in 
+     * the intendet way
+     **/  
 
-  int setFrequencyH(const unsigned short& frequency);
-  int setPosttrigH(const unsigned short& bits);
-  int setPretrigH(const unsigned short& bits);
+    int setFrequencyH(const unsigned short& frequency);
+    int setPosttrigH(const unsigned short& bits);
+    int setPretrigH(const unsigned short& bits);
 
-  /**
-   *
-   **/
-  void preformTestMeasurement(); 
-
-
-
-  /*********** Trigger Functions ***********/
+    /**
+     *
+     **/
+    void preformTestMeasurement(); 
 
 
-  
-  /** Trigger related "set functions" (as above)
-   **/
-  int setTriggerTypeH( const unsigned short& type );
-  int setTriggerChannelSourceH( const unsigned short& mask );
 
-  /** Sets the preload register for all channels to the given value and 
-   * executes the load-Trigger-Threshold command. To keep track of the
-   * set value its stored in the triggerThresholdRegister vector.
-   * Returns 0 if succesfull, 1 otherwise. (at least it should)
-   **/
-  unsigned int setTriggerThresholdRegisterAll(const unsigned int threshold);
-
-
-  ///returns the content of the triggerThresholdRegister vector
-  void getTriggerThresholdRegister();
+    /*********** Trigger Functions ***********/
 
 
   
+    /** Trigger related "set functions" (as above)
+     **/
+    int setTriggerTypeH( const unsigned short& type );
+    int setTriggerChannelSourceH( const unsigned short& mask );
 
-  /*********** Readout Functions ***********/
+    /** Sets the preload register for all channels to the given value and 
+     * executes the load-Trigger-Threshold command. To keep track of the
+     * set value its stored in the triggerThresholdRegister vector.
+     * Returns 0 if succesfull, 1 otherwise. (at least it should)
+     **/
+    unsigned int setTriggerThresholdRegisterAll(const unsigned int threshold);
+
+
+    ///returns the content of the triggerThresholdRegister vector
+    void getTriggerThresholdRegister();
+
+
+    /*********** Readout Functions ***********/
 
 
 
-  ///correct Data for trigger position
-  std::vector<std::vector<int> > correctData(std::vector<int> const& dataVec);
+    ///correct Data for trigger position
+    std::vector<std::vector<int> > correctData(std::vector<int> const& dataVec);
  
 
-  /** print data to file (in the file the unit of time will be ns and the unit of
-   * amplitude will be V
-   **/
-  void printDataToFile(std::vector<int> const& dataVec, std::string fileName = "rawData");
+    /** print data to file (in the file the unit of time will be ns and the unit of
+     * amplitude will be V
+     **/
+    void printDataToFile(std::vector<int> const& dataVec, std::string fileName = "rawData");
 
 
-  void printDataToFile(std::vector<std::vector<int> > const& dataVec, std::string fileName = "Data");
+    void printDataToFile(std::vector<std::vector<int> > const& dataVec, std::string fileName = "Data");
 
- protected:
+    int getNumberOfActiveChannels(unsigned short channel_mask);
+
+protected:
 
 
 
- private:
+private:
 
-  // function to wait a short time
-  void sleepModule();
+    // function to wait a short time
+    void sleepModule();
 
-  ///Pointer to the device one works with
-  V1729a* _currentDevice;
-  ///var to set how often some functions should try something
-  int const _nb; 
-  ///vec to store the values of the triggerThresholdRegister (all, ch1, ch2, ch3, ch4)
-  std::vector<unsigned short> _triggerThresholdRegister;
+    ///Pointer to the device one works with
+    V1729a* _currentDevice;
+    ///var to set how often some functions should try something
+    int const _nb; 
+    ///vec to store the values of the triggerThresholdRegister (all, ch1, ch2, ch3, ch4)
+    std::vector<unsigned short> _triggerThresholdRegister;
 
 };//end of class definition
 

src/High-Level-functions_VME.cc
@@ -418,12 +418,7 @@ std::vector<std::vector<int> > HighLevelFunction_VME::correctData(std::vector<in
 
   //...get number of channels
   unsigned short channelMask = _currentDevice->getChannelMask();
-  unsigned int channels = 4;
-
-  if( !(channelMask & 8) ) channels--;
-  if( !(channelMask & 4) ) channels--;
-  if( !(channelMask & 2) ) channels--;
-  if( !(channelMask & 1) ) channels--;
+  const unsigned int channels = getNumberOfActiveChannels(channelMask);
 
   std::cout << "[DEBUG] -- correctData -- Number of channels: " << channels << std::endl;
 
@@ -486,13 +481,8 @@ void HighLevelFunction_VME::printDataToFile(std::vector<int> const& dataVec, std
   unsigned int channels = 4;
   
   //set nb of channels for the readout
-  if((nbOfChannels != 1) && (nbOfChannels != 2))
-  {
-    if( !(channelMask & 8) ) channels--;
-    if( !(channelMask & 4) ) channels--;
-    if( !(channelMask & 2) ) channels--;
-    if( !(channelMask & 1) ) channels--;
-
+  if((nbOfChannels != 1) && (nbOfChannels != 2)){
+      channels = getNumberOfActiveChannels(channelMask);
   }  
   
   std::cout << "[DEBUG] -- printDataToFile -- Number of channels: " << channels << std::endl;
@@ -575,12 +565,7 @@ void HighLevelFunction_VME::printDataToFile(std::vector<std::vector<int> > const
 
   //get number of channels
   const unsigned short channelMask = _currentDevice->getChannelMask();
-  unsigned int channels = 4;
-
-  if( !(channelMask & 8) ) channels--;
-  if( !(channelMask & 4) ) channels--;
-  if( !(channelMask & 2) ) channels--;
-  if( !(channelMask & 1) ) channels--;
+  const unsigned int channels = getNumberOfActiveChannels(channelMask);
 
   std::cout << "[DEBUG] -- printDataToFile -- Number of channels: " << channels << std::endl;
 
@@ -690,4 +675,21 @@ void HighLevelFunction_VME::sleepModule(){
     // macro in header
 
     std::this_thread::sleep_for(std::chrono::milliseconds(DEFAULT_FADC_SLEEP_TIME));
+}
+
+int HighLevelFunction_VME::getNumberOfActiveChannels(unsigned short channel_mask){
+    // this function calculates the number of active channels based on the
+    // the channel mask
+    // TODO: understand what makes this number different from what the FADC returns
+    // upon getNbOfChannels().
+    int channels = 4;
+
+    // based on channel mask, we remove all bits, which are not set
+    if( !(channel_mask & 8) ) channels--;
+    if( !(channel_mask & 4) ) channels--;
+    if( !(channel_mask & 2) ) channels--;
+    if( !(channel_mask & 1) ) channels--;
+
+    return channels;
+
 }

commit 479b7103a27848ff8645cfd2d044070c5fed2d3e

fix FADC readout bug introduced in bed6279
Fixes an FADC readout bug introduced previously in commit bed6279. In
waitconditions.cpp the GetAllData() call, needs to get 4 as an
argument, instead of the number of active channels.

Commented out code related to active channels, since these are not
needed therefore. However, fix the function call!

src/waitconditions.cpp
@@ -185,7 +185,9 @@ void Producer::run()
 		}
 
 		//get fadc data
-		std::vector<int> fadcData = (parent->_hvFadcManager)->F_GetAllData(channels);
+		// TODO: if the following uses 'channels' instead of 4, the FADC readout
+		// breaks! All values 0 or massively larger!
+		std::vector<int> fadcData = (parent->_hvFadcManager)->F_GetAllData(4);
 		std::cout << "saving data to parent pointer. will probably fail " << std::endl;
 		// set the data we read from the FADC as the values for our fadc pointer
 		parent->_fadcData   = fadcData;